135fa4523fc13ea3406aa3bad5a94cfcba58505e
Tom Zarr <tzarr@salesforce.com>
2025-04-24 00:50:07 -0600

    Added more code examples for day 1 exercises

---
+++ day1/exercise3/config/sites.js
@@ -0,0 +1,22 @@
+// <other sites above>
+// ...
+{
+    id: 'MULTISITE',
+    l10n: {
+        supportedCurrencies: ['USD','GBP'],
+        defaultCurrency: 'USD',
+        defaultLocale: 'en-US',
+        supportedLocales: [
+            {
+                id: 'en-US',
+                alias: 'us',
+                preferredCurrency: 'USD'
+            },
+            {
+                id: 'en-GB',
+                alias: 'gb',
+                preferredCurrency: 'GBP'
+            }
+        ]
+    }
+}
