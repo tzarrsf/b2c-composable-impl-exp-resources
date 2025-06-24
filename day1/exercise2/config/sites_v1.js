433a7979af1c093267a71cab4c520c33eba9f7f8
Tom Zarr <38293482+tzarrsf@users.noreply.github.com>
2025-06-23 18:14:12 -0600

    Update sites_v1.js

--- day1/exercise2/config/sites_v1.js
+++ day1/exercise2/config/sites_v1.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2023, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 // Provide the sites for your app. Each site includes site id, and its localization configuration.
 // You can also provide aliases for your locale. They will be used in place of your locale id when generating paths across the app
 module.exports = [
@@ -10,11 +17,10 @@ module.exports = [
             supportedLocales: [
                 {
                     id: 'en-US',
-                    // alias: 'us',
+                    alias: 'us',
                     preferredCurrency: 'USD'
                 }
             ]
         }
     }
- ]
- 
+]
