4a813638cf5ffce2791835b0dafd754595e37740
Tom Zarr <38293482+tzarrsf@users.noreply.github.com>
2025-11-10 08:19:01 -0700

    Implement French localization support
    Add support for French localization in MULTISITE configuration.

---
+++ day1/exercise2/config/sites_FR.js
@@ -0,0 +1,54 @@
+// config/sites.js file containing FR localization
+
+/*
+ * Copyright (c) 2023, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+// Provide the sites for your app. Each site includes site id, and its localization configuration.
+// You can also provide aliases for your locale. They will be used in place of your locale id when generating paths across the app
+module.exports = [
+    {
+        id: 'RefArch',
+        l10n: {
+            supportedCurrencies: ['USD'],
+            defaultCurrency: 'USD',
+            defaultLocale: 'en-US',
+            supportedLocales: [
+                {
+                    id: 'en-US',
+                    alias: 'us',
+                    preferredCurrency: 'USD'
+                }
+            ]
+        }
+    },
+    {
+        id: 'MULTISITE',
+        l10n: {
+            supportedCurrencies: ['USD', 'GBP', 'EUR'],
+            defaultCurrency: 'USD',
+            supportedLocales: [
+                {
+                    id: 'en-US',
+                    alias: 'us',
+                    preferredCurrency: 'USD'
+                },
+                {
+                    id: 'en-GB',
+                    alias: 'gb',
+                    preferredCurrency: 'GBP'
+                },
+                // Add support for France
+                {
+                    id: 'fr-FR',
+                    alias: 'fr',
+                    preferredCurrency: 'EUR'
+                },
+            ],
+            defaultLocale: 'en-US'
+        }
+    }
+]
